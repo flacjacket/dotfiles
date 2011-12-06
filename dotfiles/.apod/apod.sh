5af1c6389ae8c97b6a3cdef2e7358dcdcd104127
Sean Vig <sean.v.775@gmail.com>
2011-12-06 13:01:56 -0600

    Modify apod.sh for laptop

--- dotfiles/.apod/apod.sh
+++ dotfiles/.apod/apod.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-clean_exit () {
+clean () {
 	[ -e index.html ] && rm index.html
 	[ -e apod.jpg ] && rm apod.jpg
-	[ -e candh-tmp.png ] && rm candh-tmp.png
+	[ -e candh-tmp1.png ] && rm candh-tmp1.png
+	[ -e candh-tmp2.png ] && rm candh-tmp2.png
 	exit
 }
 
@@ -12,32 +13,36 @@ cd ~/.apod/
 wget http://apod.nasa.gov/apod/ -O index.html -q
 if [ $? -ne 0 ]; then
 	echo "$(date '+%d/%m/%y'): FAIL - Unable to fetch webpage"
-	clean_exit
+	clean
+	exit
 fi
 
 img=`grep "<a href=\"image" index.html | cut -d\" -f 2`
 if [ -z "$img" ]; then
 	echo "$(date '+%d/%m/%y'): FAIL - Unable to find image"
-	clean_exit
+	clean
+	exit
 fi
 
 wget "http://antwrp.gsfc.nasa.gov/apod/$img" -O apod.jpg -q
 if [ $? -ne 0 ]; then
 	echo "$(date '+%d/%m/%y'): FAIL - Unable to fetch image"
-	clean_exit
+	clean
+	exit
 fi
 
 resolution=$(xdpyinfo | grep dimensions | perl -ne 'm/([0-9]{3,})x([0-9]{3,})/; print "$1x$2\n";' | head -n1 )
 convert -resize $resolution apod.jpg apod.png
+convert -resize $resolution candh.png candh-tmp1.png
 
 w=$(identify -format "%w" apod.png)
 h=$(identify -format "%h" apod.png)
-convert -gravity south -extent ${w}x${h} -background none candh.png candh-tmp.png
+convert -gravity south -extent ${w}x${h} -background none candh-tmp1.png candh-tmp2.png
 
-composite -gravity center candh-tmp.png apod.png apod.png
+composite -gravity center candh-tmp2.png apod.png apod.png
 
 [ "$(w | grep xmonad)" ] && feh --bg-center apod.png
 
 echo "$(date '+%d/%m/%y'): Success"
 
-clean_exit
+clean
