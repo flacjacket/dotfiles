4020940b7bffc4e4791dca7848dae23925677dea
Sean Vig <sean.v.775@gmail.com>
2012-01-18 11:21:14 -0600

    New apod.sh, better handling when X not running

--- dotfiles/.apod/apod.sh
+++ dotfiles/.apod/apod.sh
@@ -7,41 +7,55 @@ clean () {
 	[ -e candh-tmp2.png ] && rm candh-tmp2.png
 }
 
+log () {
+	echo "$(date '+%d/%m/%y'): $1"
+}
+
 cd ~/.apod/
 
 wget http://apod.nasa.gov/apod/ -O index.html -q
 if [ $? -ne 0 ]; then
-	echo "$(date '+%d/%m/%y'): FAIL - Unable to fetch webpage"
+	log "FAIL - Unable to fetch webpage"
 	clean
 	exit
 fi
 
 img=`grep "<a href=\"image" index.html | cut -d\" -f 2`
 if [ -z "$img" ]; then
-	echo "$(date '+%d/%m/%y'): FAIL - Unable to find image"
+	log "FAIL - Unable to find image"
 	clean
 	exit
 fi
 
 wget "http://antwrp.gsfc.nasa.gov/apod/$img" -O apod.jpg -q
 if [ $? -ne 0 ]; then
-	echo "$(date '+%d/%m/%y'): FAIL - Unable to fetch image"
+	log "FAIL - Unable to fetch image"
 	clean
 	exit
 fi
 
-resolution=$(xdpyinfo | grep dimensions | perl -ne 'm/([0-9]{3,})x([0-9]{3,})/; print "$1x$2\n";' | head -n1 )
-convert -resize $resolution apod.jpg apod.png
-convert -resize $resolution candh.png candh-tmp1.png
+if $(xdpyinfo > /dev/null 2>&1); then
+	echo "in"
+	WIDTH=$(xdpyinfo | grep dimensions | perl -ne 'm/([0-9]{3,})x([0-9]{3,})/; print "$1";' | head -n1 )
+	HEIGHT=$(xdpyinfo | grep dimensions | perl -ne 'm/([0-9]{3,})x([0-9]{3,})/; print "$2";' | head -n1 )
+else
+	if [ -e default_size.sh ]; then
+		source ./default_size.sh
+	else
+		log "FAIL - Cannot find screen size"
+		clean
+		exit
+	fi
+fi
 
-w=$(identify -format "%w" apod.png)
-h=$(identify -format "%h" apod.png)
-convert -gravity south -extent ${w}x${h} -background none candh-tmp1.png candh-tmp2.png
+convert -resize "${WIDTH}x${HEIGHT}" apod.jpg apod.png
+convert -resize "${WIDTH}x${HEIGHT}" candh.png candh-tmp1.png
+
+convert -gravity south -extent ${WIDTH}x${HEIGHT} -background none candh-tmp1.png candh-tmp2.png
 
 composite -gravity center candh-tmp2.png apod.png apod.png
 
 [ "$(w | grep xmonad)" ] && feh --bg-center apod.png
 
-echo "$(date '+%d/%m/%y'): Success"
-
+log "Success"
 clean
