02c9e9bb27ccb17293efc4e26ce0100415de7b23
Sean Vig <sean.v.775@gmail.com>
2014-07-29 20:43:19 -0500

    Import os module

--- ipython-profiles/ipython_labwork_config.py
+++ ipython-profiles/ipython_labwork_config.py
@@ -17,6 +17,7 @@ app = c.InteractiveShellApp
 load_subconfig('ipython_config.py', profile='default')
 load_subconfig('ipython_config.py', profile='sympy')
 
+app.exec_lines.append('import os')
 app.exec_lines.append('import numpy as np')
 app.exec_lines.append('import scipy as sp')
 app.exec_lines.append('import pandas as pd')
