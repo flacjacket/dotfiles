0756b0c8301d0861e57e489b5b13d03996df909f
Sean Vig <sean.v.775@gmail.com>
2015-09-03 15:48:42 -0500

    A bit nicer jupyter kernel config

--- jupyter/ipython_kernel_config.py
+++ jupyter/ipython_kernel_config.py
@@ -9,22 +9,25 @@ else:
 nbdir = os.path.expanduser(nbdir)
 nbdir = os.path.normpath(nbdir)
 
-c = get_config()
-app = c.InteractiveShellApp
+sys.path.append(nbdir)
 
 load_subconfig('ipython_config.py')
 
-c.IPKernelApp.matplotlib = 'inline'
+c = get_config()
+app = c.IPKernelApp
 
-app.exec_lines.append('import os')
+app.matplotlib = 'inline'
+
 app.exec_lines.append('import numpy as np')
 app.exec_lines.append('import scipy as sp')
 app.exec_lines.append('import pandas as pd')
 app.exec_lines.append('import matplotlib as mpl')
 app.exec_lines.append('import matplotlib.pyplot as plt')
-app.exec_lines.append("nbdir = r'%s'" % nbdir)
+app.exec_lines.append("nbdir = r'%s'".format(nbdir))
 
 app.exec_lines.append("from sympy import init_printing")
 
 if os.name == 'nt':
     app.exec_lines.append("init_printing(use_latex='mathjax')")
+else:
+    app.exec_lines.append("init_printing(use_latex=True)")
